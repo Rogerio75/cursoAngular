39efe6a132163421fa522c0adc67d5c434111e3f
rogerio75 <rogerio.ifg@gmail.com>
2023-09-23 11:57:26 -0300

    Final aula ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
  template: `
-   <app-diretivas-atributos></app-diretivas-atributos>
+   <app-diretivas-atributos>
+    <h1> Aulas de Diretivas Atributo </h1>
+     <h3>Final da Aula </h3>
+
+     <h1>My name is Roger </h1>
+     <h3> Fazendo um Piao </h3>
+   </app-diretivas-atributos>
    <router-outlet></router-outlet>`
 })
 export class AppComponent implements OnInit {
--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasAtributosComponent implements OnInit {
 public valor:boolean = true;
-public heigthPx: string = "200px";
+public heightPx: string = "200px";
 public backgroundColor: string = "red";
+public nome:string = "";
+public list:Array <{nome:string }> = [{nome: "Rogerio"}];
+
 ngOnInit(): void {
 
   setInterval( ()=>{
@@ -19,17 +22,24 @@ ngOnInit(): void {
   }
 
 
- if(this.heigthPx == "200px"){
- console.log('Alerte');
-this.heigthPx="50px";
- }else{
-
- this.heigthPx ="200px";
+ if(this.heightPx == "200px"){
+this.heightPx="50px";
+this.backgroundColor ="blue"
+}else{
+this.backgroundColor ="green"
+ this.heightPx ="200px";
  }
 
 
   },2000);
 }
+
+public salvar(){
+  this.list.push ({nome: this.nome});
+  this.nome = "";
+ }
+
+
 
 
 }
