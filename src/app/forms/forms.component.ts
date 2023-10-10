73b119af0f2e4e854fbd89c37b31e637bf38c754
Rogerio75 <rogerio.ifg@gmail.com>
2023-10-09 22:34:01 -0300

    tes

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -6,12 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent {
-public listComidas: Array<{comida: string}> =[
-{comida : "x-salada"},
-{comida : "x-picanha"},
-{comida : "x-bacon"}
+public listComidas: Array<{comida: string, preco: string}> =[
+{comida : "x-salada", preco: "R$ 20,00"},
+{comida : "x-picanha" , preco: "R$ 30,00" },
+{comida : "x-bacon" , preco: "R$ 25,00" }
 ];
 
+public submitForm(form: NgForm ){
+
+if(form.valid){
+  console.log(form.value);}
+
+
+}
 
 
 }
